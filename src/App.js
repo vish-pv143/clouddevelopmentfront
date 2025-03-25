5be16ca8b04f97792ad0146b472dc1f84136bd64
Avanti <kantumuchuavanthi@gmail.com>
2025-03-25 14:01:15 -0500

    replaced the api url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/api/students')
+    fetch('https://clouddevbackend-e9f7g2h3hdhrbpde.centralus-01.azurewebsites.net/api/students')
       .then(res => res.json())
       .then(data => setStudents(data))
       .catch(err => console.error('Error fetching students:', err));
