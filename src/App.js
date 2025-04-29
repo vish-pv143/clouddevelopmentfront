35fb683a8802fd3c41cee56462b6a4e090feb190
vish-pv143 <vishnu.palithya@gmail.com>
2025-04-29 14:27:20 -0500

    Added files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    fetch('https://clouddev3.azurewebsites.net/api/students')
+    fetch('https://vishnuwebapp-gkc8grhthbbcgcbd.centralus-01.azurewebsites.net/api/students')
       .then(res => res.json())
       .then(data => setStudents(data))
       .catch(err => console.error('Error fetching students:', err));
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Student List of Avanti Kantumuchu</h1>
+      <h1>Student List of Vishnu</h1>
       <table border="1" cellPadding="10">
         <thead>
           <tr>
